464a0562d1becf3e01e2982156d49d01dc736f13
Darshan Raut <56134448+darshanraut123@users.noreply.github.com>
2021-12-09 23:03:55 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+const data = [
+    {
+      id:1,
+      offer: "sale",
+      title: "Special Item",
+      originalPrice: 20.00,
+      discountedPrice:18.00,
+      stars: [1,2,3,4,5],
+      btnStatus: false
+    },
+    {
+      id:2,
+      offer: "sale",
+      title: "Sale Item",
+      originalPrice: 50.00,
+      stars: [],
+      discountedPrice: 25.00,
+      btnStatus: false,
+    },
+    {
+      id:3,
+      offer: "",
+      title: "Popular Item",
+      originalPrice: null,
+      stars: [1,2,3,4,5],
+      discountedPrice: 40.00,
+      btnStatus: false
+    },
+    {
+      id:4,
+      offer: "sale",
+      title: "Sale Item",
+      originalPrice: 50.00,
+      stars: [],
+      discountedPrice: 25.00,
+      btnStatus: false,
+    },
+    {
+      id:5,
+      offer: "sale",
+      title: "Special Item",
+      originalPrice: 20.00,
+      stars: [1,2,3],
+      discountedPrice: 18.00,
+      btnStatus: false,
+    },
+    {
+      id:6,
+      offer: "",
+      title: "Popular Item",
+      originalPrice: null,
+      stars: [1,2],
+      discountedPrice: 40.00,
+      btnStatus: false,
+    }
+  ];
+
+  export default data;
